fecccfb15ed768c3ef296c213d7700189d52eb13
BuildTools <unconfigured@null.spigotmc.org>
2017-09-26 19:17:01 +0700

    Moved move() method to the Entity base class

--- entity/Entity.lua
+++ entity/Entity.lua
@@ -21,4 +21,10 @@ function Entity:initialize(id, world, x, y)
 	self.pathGraph = PathGraph:new(world.staticObjects)
 end
 
+function Entity:move(dx, dy)
+  	if dx ~= 0 or dy ~= 0 then
+  	  	self.x, self.y, cols, cols_len = self.world:move(self, self.x + dx, self.y + dy)
+  	end
+end
+
 return Entity
